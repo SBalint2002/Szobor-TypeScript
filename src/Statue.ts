fe3c30a88739053d5d36e5843fa52ac9a0217769
Sárándi <sarandi.balint@gmail.com>
2022-11-09 12:02:38 +0100

    Base files, interface, class

---
+++ src/Statue.ts
@@ -0,0 +1,22 @@
+import { Artwork } from "./Artwork";
+
+export class Statue implements Artwork {
+  title: string;
+  year: number;
+  price: number;
+  height: number;
+
+  constructor(title: string, year: number, price: number, height: number) {
+    this.title = title;
+    this.year = year;
+    this.price = price;
+    this.height = height;
+  }
+
+  kiir(): void {
+    console.log(this.toString());
+  }
+  toString(): string {
+    return this.title + " " + this.year + " " + this.price + " Ft";
+  }
+}
