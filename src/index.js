87595a654569031c55b7765288b2350f2bd1d9c7
Sárándi <sarandi.balint@gmail.com>
2022-11-09 12:30:28 +0100

    adatok ellenőrzése

--- src/index.js
+++ src/index.js
@@ -1,5 +1,70 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const Statue_1 = require("./Statue");
+let nevPattern = /^[aA-zZ, ]{1,}$/;
 let szobrok = [];
 document.addEventListener('DOMContentLoaded', () => {
+    var _a;
+    (_a = document.getElementById('gomb')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => {
+        //Cím ellenőrzés
+        let jocim = false;
+        let cim = document.getElementById('nevinput');
+        if (nevPattern.test(cim.value)) {
+            joAdat(cim);
+            jocim = true;
+        }
+        else {
+            rosszAdat(cim);
+            jocim = false;
+        }
+        //Évszám ellenőrzés
+        let joev = false;
+        let ev = document.getElementById('evszaminput');
+        if (parseInt(ev.value) < 2023) {
+            joAdat(ev);
+            joev = true;
+        }
+        else {
+            rosszAdat(ev);
+            joev = false;
+        }
+        //Ár ellenőrzés
+        let joar = false;
+        let ar = document.getElementById('arinput');
+        if (parseInt(ar.value) > 0) {
+            joAdat(ar);
+            joar = true;
+        }
+        else {
+            rosszAdat(ar);
+            joar = false;
+        }
+        //magasság ellenőrzés
+        let jomagassag = false;
+        let magassag = document.getElementById('magassaginput');
+        if (parseInt(magassag.value) > 9) {
+            joAdat(magassag);
+            jomagassag = true;
+        }
+        else {
+            rosszAdat(magassag);
+            jomagassag = false;
+        }
+        //Feltöltés + összesítés
+        if (jocim && joev && joev && jomagassag) {
+            szobrok.push(new Statue_1.Statue(cim.value, parseInt(ev.value), parseInt(ar.value), parseInt(magassag.value)));
+            cim.value = "";
+            ev.value = "";
+            ar.value = "";
+            magassag.value = "";
+        }
+    });
+    //rossz adat esetén piros border + ellenorzo
+    function rosszAdat(adat) {
+        adat.style.border = "1px solid red";
+    }
+    //joadat esetén alapértelmezett border + ellenorzo
+    function joAdat(adat) {
+        adat.style.border = "1px solid #ced4da";
+    }
 });
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,76 @@
 import { Artwork } from "./Artwork";
 import { Statue } from "./Statue";
 
+let nevPattern = /^[aA-zZ, ]{1,}$/;
+
 let szobrok: Artwork[] = [];
 
 document.addEventListener('DOMContentLoaded', () => {
-    
+    document.getElementById('gomb')?.addEventListener('click', () =>{
+
+        //Cím ellenőrzés
+        let jocim = false;
+        let cim : HTMLInputElement = document.getElementById('nevinput')as HTMLInputElement;
+        if(nevPattern.test(cim.value)){
+            joAdat(cim);
+            jocim = true;
+        }else{
+            rosszAdat(cim);
+            jocim = false;
+        }
+
+        //Évszám ellenőrzés
+        let joev = false;
+        let ev : HTMLInputElement = document.getElementById('evszaminput')as HTMLInputElement;
+        if(parseInt(ev.value) < 2023){
+            joAdat(ev)
+            joev = true;
+        }else{
+            rosszAdat(ev);
+            joev = false;
+        }
+
+        //Ár ellenőrzés
+        let joar = false;
+        let ar : HTMLInputElement = document.getElementById('arinput')as HTMLInputElement;
+        if(parseInt(ar.value) > 0){
+            joAdat(ar)
+            joar = true;
+        }else{
+            rosszAdat(ar);
+            joar = false;
+        }
+
+        //magasság ellenőrzés
+        let jomagassag = false;
+        let magassag : HTMLInputElement = document.getElementById('magassaginput')as HTMLInputElement;
+        if(parseInt(magassag.value) > 9){
+            joAdat(magassag)
+            jomagassag = true;
+        }else{
+            rosszAdat(magassag);
+            jomagassag = false;
+        }
+
+        //Feltöltés + összesítés
+        if(jocim && joev && joev && jomagassag){
+            szobrok.push(new Statue(cim.value,parseInt(ev.value), parseInt(ar.value), parseInt(magassag.value)));
+            cim.value = "";
+            ev.value = "";
+            ar.value = "";
+            magassag.value = "";
+        }
+
+
+    })
+
+    //rossz adat esetén piros border + ellenorzo
+    function rosszAdat (adat : HTMLInputElement){
+        adat.style.border = "1px solid red";
+    }
+
+    //joadat esetén alapértelmezett border + ellenorzo
+    function joAdat(adat: HTMLInputElement){
+        adat.style.border = "1px solid #ced4da";
+    }
 })
